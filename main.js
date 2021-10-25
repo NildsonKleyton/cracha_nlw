455f4296c12f7aaa97120be454878f59648cd25e
NildsonKleyton <nildsonkleyton@hotmail.com>
2021-10-25 20:53:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function getGitHubProfileInfos() {
       userName.textContent = data.name
       userBio.textContent = data.bio
       userLink.href = data.html_url
-      //userImage.src = data.avatar_url
+      userImage.src = data.avatar_url
       userLogin.textContent = data.login
     })
 
